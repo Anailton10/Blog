3268d1ff82bafe280d0e3da46a9b8a0d7820f83a
Anailton Silva <francisco.anailton17@gmail.com>
2024-02-26 00:22:37 -0300

    feat: register in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from .models import BlogPost, Category
+
+
 # Register your models here.
+@admin.register(BlogPost)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "category",
+        "created_at"
+    )
+    search_fields = (
+        "title",
+        "author",
+        "category"
+    )
+
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
