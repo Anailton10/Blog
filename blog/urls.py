d86f7bf90009ba40487097d3f1b6fab4420604ae
Anailton Silva <francisco.anailton17@gmail.com>
2024-02-25 23:59:24 -0300

    feat: url create

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from blog import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("detail/<int:pk>/", views.detail, name="detail"),
-    
+    path("create/", views.create_post, name="create"),
 ]
