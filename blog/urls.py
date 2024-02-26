2f5f546b6b1db988170e03159284062458b0aaec
Anailton Silva <francisco.anailton17@gmail.com>
2024-02-25 23:58:43 -0300

    feat: urls for views

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from blog import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("detail/<int:pk>/", views.detail, name="detail"),
+    
+]
