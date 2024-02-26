279a57868c79abba55b87545e2d91053bc3ff3ae
Anailton Silva <francisco.anailton17@gmail.com>
2024-02-26 13:38:34 -0300

    feat: create model

--- blog/models.py
+++ blog/models.py
@@ -10,15 +10,25 @@ class Category(models.Model):
         return str(self.name)
 
 
+class Language(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.name)
+
+
 class BlogPost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=300, blank=True, null=True)
     content = models.TextField()
     created_at = models.DateField(auto_now_add=True)
     update_at = models.DateField(auto_now=True)
     category = models.ForeignKey(
         Category, on_delete=models.PROTECT, blank=True, null=True
+    )
+    language = models.ForeignKey(
+        Language, on_delete=models.PROTECT, blank=True, null=True
     )
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
-from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import redirect, render
 
 from .forms import CreateBlogPostForm
 from .models import BlogPost
+
+# TODO: categorias e linguagem ver possibilidades para filtragem
 
 
 # Create your views here.
@@ -29,7 +31,7 @@ def create_post(request):
         post_form = CreateBlogPostForm(request.POST, request.FILES)
         if post_form.is_valid():
             post_form.save()
-            return redirect('home')
+            return redirect("home")
         else:
             return render(request, "register_post.html", {"post_form": post_form})
     else:
