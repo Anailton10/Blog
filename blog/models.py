3aa24180668a22477a3a0b3d131d53b5d83995d6
Anailton Silva <francisco.anailton17@gmail.com>
2024-02-25 23:56:27 -0300

    feat: models Catgory and BlogPost

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class BlogPost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, blank=True, null=True)
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, blank=True, null=True
+    )
+
+    def __str__(self):
+        return str(self.author)
